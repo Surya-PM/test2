8f010740e155bcb5012fdfa64f206b7805580454
Surya <87151238+Surya-PM@users.noreply.github.com>
2021-10-22 20:56:08 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {  
+ const name="Dhoni";
+  return (
+    <div className="App">
+ 
+  <h1>Hi {name}😎!!!</h1>
+    </div>
+  );
+}
+
+
+
+
+
+export default App;
